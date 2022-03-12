9c3345ae22370031c3cf46f87e09b5a2463f8107
Gustavo-Zamboni <gamegutaz@gmail.com>
2022-03-12 13:07:02 -0300

    Manipulando jSON

--- async_await/index.js
+++ async_await/index.js
@@ -1,3 +1,5 @@
+const { default: axios } = require('axios')
+
 require('dotenv').config()
 
 const PROTOCOL = process.env.PROTOCOL
@@ -12,4 +14,32 @@ const Q = process.env.Q
 
 const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
 
-console.log(url)
+//IO-BOUND 
+axios.get(url)
+.then(res => {
+    console.log(res.data)
+    return res.data
+})
+.then(res => {
+    console.log(res.cnt)
+    return res
+})
+.then(res => {
+    return res['list']
+})
+.then(res => {
+    for(let previsao of res){
+        console.log(`
+            ${new Date (+previsao.dt * 1000).toLocaleString()},
+            Min: ${previsao.main.temp_min}\u00B0C,
+            Min: ${previsao.main.temp_max}\u00B0C,
+            Hum: ${previsao.main.humidity}%,
+            ${previsao.weather[0].description}
+        `)
+    }
+})
+.then(res => {
+    //exibir a qunatidade de previsões que têm percepção humana 
+    //de temperatura acima de 30 graus
+    
+})
