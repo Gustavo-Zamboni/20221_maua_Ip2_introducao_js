c2ea67e36c7777512acdc13108ca96cc5f1cf082
Gustavo-Zamboni <gamegutaz@gmail.com>
2022-04-02 13:05:10 -0300

    Implementacao das funcoes do barramento de eventos

--- mss_lembrete_obs/barramento de eventos/index.js
+++ mss_lembrete_obs/barramento de eventos/index.js
@@ -4,12 +4,18 @@ const axios = require('axios')
 const app = express()
 app.use(express.json())
 
-app.post('/eventos', (req, res) => {
+app.post('/eventos', async (req, res) => {
     const evento = req.body
-
-    axios.post('localhost:4000/eventos', evento)
-    axios.post('localhost:5000/eventos', evento)
-    res.status(201).json({msg: "ok"})
+    try {
+        await axios.post('http://localhost:4000/eventos', evento)
+        await axios.post('http://localhost:5000/eventos', evento)  
+        res.status(201).json({msg: "ok"})
+    
+    } catch (error) {
+        console.log(error)
+        res.status(500).end()
+    }
 })
 
 app.listen(10000, () => console.log("Barramento de eventos. Porta 10000"))
+
